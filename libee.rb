de8c98a0bfa5555d4e20fbbb606c9e0e2cd6f5ca
Sven Bange <sven.bange@spurtreu.de>
2014-08-12 21:46:31 +0200

    fix: new formulas fpr libee and libestr

--- libee.rb
+++ libee.rb
@@ -1,18 +1,18 @@
-require "formula"
+require 'formula'
 
 class Libee < Formula
-  homepage "http://www.libee.org"
-  url "http://www.libee.org/download/files/download/libee-0.4.1.tar.gz"
-  sha1 "06bcd1be5c60e60a1e8d9a5844b3e9edc76b14e3"
+  homepage 'http://www.libee.org/'
+  url 'http://www.libee.org/download/files/download/libee-0.4.1.tar.gz'
+  sha1 '06bcd1be5c60e60a1e8d9a5844b3e9edc76b14e3'
 
-  depends_on 'pkg-config' => :build 
+  depends_on 'sbange/rsyslog-elasticsearch/libestr'
 
   def install
+    ENV.j1
     ENV.deparallelize
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-silent-rules"
+    system "make"
     system "make", "install"
   end
 
--- libestr.rb
+++ libestr.rb
@@ -1,25 +1,12 @@
 require 'formula'
 
 class Libestr < Formula
-  homepage 'http://libestr.adiscon.com'
-  url "http://libestr.adiscon.com/files/download/libestr-0.1.9.tar.gz"
-  sha1 "55fb6ad347f987c45d98b422b0436d6ae50d86aa"
-
-  bottle do
-    cellar :any
-    sha1 "bc0dc2d4fc17dce269003705c01cda62482b58a3" => :mavericks
-    sha1 "e232f60fdda3548f5f39b48221a018b363554e8c" => :mountain_lion
-    sha1 "e955560fd837eacbc843c631a1e5aaa432802942" => :lion
-  end
-
-  depends_on 'pkg-config' => :build
-
-  option :universal
+  homepage 'http://libestr.adiscon.com/'
+  url 'http://libestr.adiscon.com/files/download/libestr-0.1.9.tar.gz'
+  sha256 '822c6e2d01eaca1e72201f403a2ca01f3e86410b880e508e5204e3c2694d751a'
 
   def install
-    ENV.universal_binary if build.universal?
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make"
-    system "make install"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
