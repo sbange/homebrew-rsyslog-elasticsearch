feaea6df099a07868398db5254a605616756a6f9
Sven Bange <sven.bange@spurtreu.de>
2014-08-12 21:48:08 +0200

    fix: format

--- libee.rb
+++ libee.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Libee < Formula
+
   homepage 'http://www.libee.org/'
   url 'http://www.libee.org/download/files/download/libee-0.4.1.tar.gz'
   sha1 '06bcd1be5c60e60a1e8d9a5844b3e9edc76b14e3'
--- libestr.rb
+++ libestr.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class Libestr < Formula
+  
   homepage 'http://libestr.adiscon.com/'
   url 'http://libestr.adiscon.com/files/download/libestr-0.1.9.tar.gz'
   sha256 '822c6e2d01eaca1e72201f403a2ca01f3e86410b880e508e5204e3c2694d751a'
@@ -9,4 +10,5 @@ class Libestr < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
+
 end
